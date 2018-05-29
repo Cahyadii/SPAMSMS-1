3342e104a445e21ac510eb834b44ea732d953716
Nazirr <39704763+Nazirr@users.noreply.github.com>
2018-05-29 23:02:01 +0700

    Update SPAMSMS.sh

--- SPAMSMS.sh
+++ SPAMSMS.sh
@@ -18,7 +18,7 @@ echo "4. PHD"
 echo "5. Jd.Id"
 echo "6. Email Bomber"
 echo "7. Hooq"
-echo "8. Grab
+echo "8. Grab"
 echo "0. Keluar"
 echo "\033[30;1m81. Informasi Tool Ini"
 echo "\033[33;1m Pilih Angka:"
